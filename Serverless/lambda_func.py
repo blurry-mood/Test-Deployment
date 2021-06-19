0a68462aae4939e21f5a9da447a14bc0249313fd
blurry-mood <assis.ayoub@gmail.com>
2021-06-19 09:43:02 +0100

    finalize the code

--- Serverless/lambda_func.py
+++ Serverless/lambda_func.py
@@ -6,44 +6,22 @@ endpoint_name ='torchserve-endpoint-2021-06-06-21-56-06'
 session = boto3.Session(aws_access_key_id='xxxxx', aws_secret_access_key='/dgdgdf gdgvdfg dfgdf +cK')
 s3 = session.client('s3')
 sage = session.client('runtime.sagemaker')
-
-def s3_read(bucket, key):
-    """
-    Read a file from an S3 source.
-
-    Parameters
-    ----------
-    source : str
-        Path starting with s3://, e.g. 's3://bucket-name/key/foo.bar'
-    profile_name : str, optional
-        AWS profile
-
-    Returns
-    -------
-    content : bytes
-
-    botocore.exceptions.NoCredentialsError
-        Botocore is not able to find your credentials. Either specify
-        profile_name or add the environment variables AWS_ACCESS_KEY_ID,
-        AWS_SECRET_ACCESS_KEY and AWS_SESSION_TOKEN.
-        See https://boto3.readthedocs.io/en/latest/guide/configuration.html
-    """
-    s3_object = s3.get_object(Bucket=bucket_name, Key=key)
-    body = s3_object['Body']
-    return body.read()
     
 bucket_name = 'uploaderr'
 
 def lambda_handler(event, context):
     
+    # Read the input image
     body = json.loads(event['body'])
     im_b64 = body['image']
     payload = base64.b64decode(im_b64.encode('utf-8'))
     
+    # Pass the image to the model & wait for the results
     response = sage.invoke_endpoint(EndpointName=endpoint_name, 
                                    ContentType='application/x-image', 
                                    Body=payload)
-        
+    
+    # Convert list of int to list of strings
     a = [str(t) for t in json.loads(response['Body'].read()) ]
     
     return {
